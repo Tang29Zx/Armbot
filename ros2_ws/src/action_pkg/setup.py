5e2f176dd9784ac57f2667012ffc3079b748bfb1
Tang29Zx <Tang29Zx@outlook.com>
2026-07-12 14:29:25 +0800

    机械臂控制代码实现

--- ros2_ws/src/action_pkg/setup.py
+++ ros2_ws/src/action_pkg/setup.py
@@ -1,4 +1,6 @@
 from setuptools import find_packages, setup
+import os
+from glob import glob
 
 package_name = 'action_pkg'
 
@@ -10,17 +12,23 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        # Traceable config (contract sec 4)
+        (os.path.join('share', package_name, 'config'), glob('config/*.yaml')),
+        # Launch files
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='sunrise',
     maintainer_email='sunrise@todo.todo',
-    description='TODO: Package description',
+    description='Arm/gripper control node implementing the stable ROS2 interface contract',
     license='Apache-2.0',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'i2c_controller = action_pkg.i2c_controller:main',
+            'arm_controller = action_pkg.arm_controller_node:main',
+            # Backward-compatible alias for the old i2c_controller entry point.
+            'i2c_controller = action_pkg.arm_controller_node:main',
         ],
     },
 )
